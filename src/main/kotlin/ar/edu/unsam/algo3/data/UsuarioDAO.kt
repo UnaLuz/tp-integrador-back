b7261242496a144b280ec768da0a4653db0d080e
UnaLuz <luz.cucarella97@gmail.com>
2022-11-11 12:54:26 -0300

    Update queries and add delete, update and selectOne methods to UsuarioDAO

--- src/main/kotlin/ar/edu/unsam/algo3/data/UsuarioDAO.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/data/UsuarioDAO.kt
@@ -5,10 +5,30 @@ import java.sql.ResultSet
 
 object UsuarioDAO {
     private val TAG = this::class.simpleName.toString()
-    private const val SELECT = "SELECT id_usuario, nombre, apellido, password, fecha_nacimiento FROM usuario;"
+
+    // DB info
+    const val DB_TABLE = "usuario"
+    const val COL_ID_USUARIO = "id_usuario"
+    const val COL_NOMBRE = "nombre"
+    const val COL_APELLIDO = "apellido"
+    const val COL_PASSWORD = "password"
+    const val COL_FECHA_NACIMIENTO = "fecha_nacimiento"
+
+    // Queries
+    private const val SELECT =
+        "SELECT $COL_ID_USUARIO, $COL_NOMBRE, $COL_APELLIDO, $COL_PASSWORD, $COL_FECHA_NACIMIENTO FROM $DB_TABLE;"
     private const val INSERT =
-        "INSERT INTO usuario(nombre, apellido, password, fecha_nacimiento) VALUES(?, ?, ?, ?);"
+        "INSERT INTO $DB_TABLE($COL_NOMBRE, $COL_APELLIDO, $COL_PASSWORD, $COL_FECHA_NACIMIENTO) VALUES(?, ?, ?, ?);"
+    private const val UPDATE =
+        "UPDATE $DB_TABLE SET $COL_NOMBRE = ?, $COL_APELLIDO = ?, $COL_PASSWORD = ?, $COL_FECHA_NACIMIENTO = ? WHERE $COL_ID_USUARIO = ?;"
+    private const val DELETE =
+        "DELETE FROM $DB_TABLE WHERE $COL_ID_USUARIO = ?;"
+    private const val SELECT_ONE =
+        "SELECT $COL_ID_USUARIO, $COL_NOMBRE, $COL_APELLIDO, $COL_PASSWORD, $COL_FECHA_NACIMIENTO FROM $DB_TABLE WHERE $COL_ID_USUARIO = ?;"
+
+    // Errores
     const val DB_ERROR = -1
+    const val USER_ERROR = -2
 
     /**
      * Realiza un select de todos los usuarios de la base de datos.
@@ -30,21 +50,49 @@ object UsuarioDAO {
      * Si alguno de los campos es nulo, se utiliza un string vacío.
      */
     fun insert(usuario: Usuario): Int =
-        DBConnection.executeUpdate(
-            INSERT
-        ) {
+        DBConnection.executeUpdate(INSERT) {
             it.setString(1, usuario.nombre.orEmpty())
             it.setString(2, usuario.apellido.orEmpty())
             it.setString(3, usuario.password.orEmpty())
             it.setString(4, usuario.fechaNacimiento.orEmpty())
         } ?: DB_ERROR
+
+    fun update(usuario: Usuario): Int {
+        return usuario.id?.let { id ->
+            DBConnection.executeUpdate(UPDATE) {
+                it.setString(1, usuario.nombre.orEmpty())
+                it.setString(2, usuario.apellido.orEmpty())
+                it.setString(3, usuario.password.orEmpty())
+                it.setString(4, usuario.fechaNacimiento.orEmpty())
+                it.setInt(5, id)
+            } ?: DB_ERROR
+        } ?: USER_ERROR
+    }
+
+    fun delete(usuario: Usuario): Int {
+        return usuario.id?.let { id ->
+            DBConnection.executeUpdate(DELETE) {
+                it.setInt(1, id)
+            } ?: DB_ERROR
+        } ?: USER_ERROR
+    }
+
+    fun selectOne(usuario: Usuario): Usuario? {
+        return usuario.id?.let { id ->
+            DBConnection.executeQuery(SELECT_ONE, {
+                it.setInt(1, id)
+            }) { resultSet ->
+                if(resultSet.next()) resultSet.mapToUsuario() else null
+            }
+        }
+    }
 }
 
 private fun ResultSet.mapToUsuario() =
     Usuario(
-        id = getInt("id_usuario"),
-        nombre = getString("nombre"),
-        apellido = getString("apellido"),
-        password = getString("password"),
-        fechaNacimiento = getString("fecha_nacimiento")
+        id = getInt(UsuarioDAO.COL_ID_USUARIO),
+        nombre = getString(UsuarioDAO.COL_NOMBRE),
+        apellido = getString(UsuarioDAO.COL_APELLIDO),
+        password = getString(UsuarioDAO.COL_PASSWORD),
+        fechaNacimiento = getString(UsuarioDAO.COL_FECHA_NACIMIENTO)
     )
--- src/test/kotlin/ar/edu/unsam/algo3/controller/TestDatabaseConnection.kt
+++ src/test/kotlin/ar/edu/unsam/algo3/controller/TestDatabaseConnection.kt
@@ -7,20 +7,43 @@ import ar.edu.unsam.algo3.utils.Logger
 const val TAG = "TestDatabaseConnection"
 
 fun main(args: Array<String>) {
-
-    Logger.debug(TAG, "--- Insert Usuario ---")
+    val id = 11
+    Logger.info(TAG, "--- Insert Usuario ---")
     val resultado = UsuarioDAO.insert(
         Usuario(
-            nombre = "Pepita",
+            nombre = "Manu",
             apellido = null,
-            password = "",
-            fechaNacimiento = "2022-11-10"
+            password = "asd",
+            fechaNacimiento = "2022-11-11"
         )
     )
     Logger.debug(TAG, "Se insertaron $resultado usuario/s")
 
-    Logger.debug(TAG, "--- Select Usuarios ---")
+    Logger.info(TAG, "--- Usuario con id=$id antes del update ---")
+    Logger.info(TAG, UsuarioDAO.selectOne(Usuario(id = id)).toString())
+
+    Logger.info(TAG, "--- Update Usuario ---")
+    val actualizados = UsuarioDAO.update(
+        Usuario(
+            id = id,
+            nombre = "Andres",
+            apellido = "ccc",
+            password = "qwerty",
+            fechaNacimiento = "2022-11-11"
+        )
+    )
+    Logger.debug(TAG, "Se actualizaron $actualizados usuario/s")
+
+    Logger.info(TAG, "--- Usuario con id=$id despues del update---")
+    Logger.info(TAG, UsuarioDAO.selectOne(Usuario(id = id)).toString())
+
+    Logger.info(TAG, "--- Delete Usuario ---")
+    Logger.info(TAG, "Eliminando usuario con id=$id")
+    val eliminados = UsuarioDAO.delete(Usuario(id = id))
+    Logger.debug(TAG, "Se eliminaron $eliminados usuario/s")
+
+    Logger.info(TAG, "--- Select Usuarios ---")
     UsuarioDAO.selectAll()?.forEach { usuario ->
-        Logger.debug(TAG, usuario.toString())
-    } ?: Logger.debug(TAG, "La lista de usuarios no pudo ser recuperada")
+        Logger.info(TAG, usuario.toString())
+    } ?: Logger.warning(TAG, "La lista de usuarios no pudo ser recuperada")
 }
