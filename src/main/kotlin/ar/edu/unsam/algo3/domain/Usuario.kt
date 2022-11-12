6a80c9c6800191b3d83bea8e4c8cfba585338b0f
UnaLuz <luz.cucarella97@gmail.com>
2022-11-12 01:04:10 -0300

    Remove unnecessary Entidad interface

--- src/main/kotlin/ar/edu/unsam/algo3/domain/Usuario.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/domain/Usuario.kt
@@ -1,9 +1,9 @@
 package ar.edu.unsam.algo3.domain
 
 data class Usuario(
-    override val id: Int? = null,
+    val id: Int? = null,
     val nombre: String? = null,
     val apellido: String? = null,
     val password: String? = null,
     val fechaNacimiento: String? = null
-): Entidad
+)
