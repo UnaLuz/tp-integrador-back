fdbea9d044cade68f24fe3ae2edcebee49fca63f
UnaLuz <luz.cucarella97@gmail.com>
2022-11-15 11:34:25 -0300

    Fix contenido inicio query

--- src/main/kotlin/ar/edu/unsam/algo3/dao/ContenidoRepository.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/dao/ContenidoRepository.kt
@@ -25,7 +25,10 @@ class ContenidoRepository() : EntidadRepository<Contenido> {
     // Queries
     val SELECT_INICIO =
         """SELECT c.$ID_CONTENIDO, c.$TITULO, avg(d.velocidad) AS $VELOCIDAD_PROM, avg(re.puntaje) AS $PUNTAJE_PROM, $PUNTAJE_MAX, $TIPO_CONTENIDO, t_mejor_puntaje.$USUARIO_RESPONDE, t_mejor_puntaje.$ID_RESPUESTA
-FROM (respuesta_encuesta re, descarga d, contenido c)
+FROM (contenido c)
+LEFT JOIN (respuesta_encuesta re, descarga d)
+ON (d.id_contenido_documento = c.id_contenido OR d.id_contenido_musica = c.id_contenido)
+AND re.id_descarga_realizada = d.id_descarga
 LEFT JOIN (
 	SELECT c.$ID_CONTENIDO, re.puntaje AS $PUNTAJE_MAX, re.$USUARIO_RESPONDE, re.id_respuesta_encuesta AS $ID_RESPUESTA
 	FROM (respuesta_encuesta re, descarga d, contenido c)
@@ -35,7 +38,7 @@ LEFT JOIN (
 	GROUP BY c.id_contenido
 ) t_mejor_puntaje
 ON c.id_contenido = t_mejor_puntaje.id_contenido
-WHERE re.id_descarga_realizada = d.id_descarga
+AND re.id_descarga_realizada = d.id_descarga
 AND (d.id_contenido_documento = c.id_contenido OR d.id_contenido_musica = c.id_contenido)
 GROUP BY c.id_contenido;"""
 
